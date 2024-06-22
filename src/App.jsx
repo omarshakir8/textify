0147514ba967d7ef953e478787e6fa5492db9641
omarshakir8 <omarshakir8@gmail.com>
2024-06-22 04:59:39 -0400

    Testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
-import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
+      <h1 className='text-green-400'>HELLO THERE</h1>
 
     </div>
   )
