43dee308c17e8a2ee8256547a3821508883d34c1
omarshakir8 <omarshakir8@gmail.com>
2024-06-22 05:28:18 -0400

    Added New Button

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,16 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='flex flex-col p-4'>
+    <div className='flex flex-col p-4 max-w- [1000px]mx-auto w-full'>
       <section className='min-h-screen flex flex-col'>
-        <header>
+        <header className='flex items-center justify-between gap-4'>
           <h1>text<span className='text-purple-900'>ify</span></h1>
+          <button className='flex items-center gap-2'>
+            <p>New</p>
+            <i className= "fa-solid fa-plus"></i>
+          </button>
+          
+          
 
 
         </header>
