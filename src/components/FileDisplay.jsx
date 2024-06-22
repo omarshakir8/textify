6e785c65e07f8141ce58a2d4f205612341099a80
omarshakir8 <omarshakir8@gmail.com>
2024-06-22 19:06:33 -0400

    Reset and Transcribe Button

--- src/components/FileDisplay.jsx
+++ src/components/FileDisplay.jsx
@@ -5,14 +5,20 @@ export default function FileDisplay(props) {
 
   return (
     <div>
-      <main className='flex-1 p-4 flex flex-col gap-3 text-center sm:gap-4 md:gap-5 justify-center pb-20'>
+      <main className='flex-1 p-4 flex flex-col gap-3 text-center sm:gap-4 md:gap-5 justify-center pb-20 w-fit max-w-full mx-auto'>
       <h1 className= 'font-semibold text-6xl sm:text-7xl md: text-8xl'>Your<span className='text-purple-900 bold'>File</span></h1>
-      <div className='mx-auto flex flex-col text-left'>
+      <div className='mx-auto flex flex-col text-left my-4'>
         <h3 className='font-semibold text-2xl'>Name</h3>
         <p className='text-2xl'>{file.name}</p>
+        </div>
 
 
 
+      <div className='flex items-center justify-between gap-4'>
+        <button className='text-slate-400'>Reset</button>
+        <button className='specialBtn px-4 py-2 rounded-lg text-purple-900'>
+          <p>Transcribe</p>
+          </button>
       </div>
 
 
