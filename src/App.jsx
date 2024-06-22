df64f87241e43a91ae9f615dc1a14c03d7447740
omarshakir8 <omarshakir8@gmail.com>
2024-06-22 05:22:16 -0400

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,20 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <h1 className='text-green-400'>HELLO THERE</h1>
+    <div className='flex flex-col p-4'>
+      <section className='min-h-screen flex flex-col'>
+        <header>
+          <h1>text<span className='text-purple-900'>ify</span></h1>
+
+
+        </header>
+        <main className='flex-1'>
+
+          </main>
+
+      </section>
+      <h1 className='text-green-400'>hello there!</h1>
+      <footer></footer>
 
     </div>
   )
