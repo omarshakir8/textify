844a6dd5a9d6bb2bf413d320532ef82c336784d7
omarshakir8 <omarshakir8@gmail.com>
2024-06-22 17:02:49 -0400

    Set AudioStream

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import { useState } from 'react'
 import HomePage from './components/HomePage.jsx'
 import Header from './components/Header.jsx'
+import FileDisplay from './components/FileDisplay.jsx'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [file, setFile] = useState(null)
+  const [audioStream, setAudioStream] = useState(null)
+
+  const isAudioAvailable = file || audioStream
 
   return (
     <div className='flex flex-col p-4 max-w-[1000px]mx-auto w-full'>
       <section className='min-h-screen flex flex-col'>
         <Header />
+       {isAudioAvailable ? (
+        <FileDisplay/>
+
+       ) : ( 
        <HomePage />
+       )}
 
       </section>
       <h1 className='text-green-400'>hello there!</h1>
