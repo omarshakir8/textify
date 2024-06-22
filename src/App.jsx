06186e93685dad9d2fdadb15c8cc4b08c1de3f4b
omarshakir8 <omarshakir8@gmail.com>
2024-06-22 05:45:48 -0400

    Interface test

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,16 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='flex flex-col p-4 max-w- [1000px]mx-auto w-full'>
+    <div className='flex flex-col p-4 max-w-[1000px]mx-auto w-full'>
       <section className='min-h-screen flex flex-col'>
-        <header className='flex items-center justify-between gap-4'>
-          <h1>text<span className='text-purple-900'>ify</span></h1>
+        <header className='flex items-center justify-between gap-4 p-4'>
+          <h3 className= 'text-2xl'>text<span className='text-purple-900'>ify</span></h3>
           <button className='flex items-center gap-2'>
             <p>New</p>
             <i className= "fa-solid fa-plus"></i>
           </button>
-          
-          
-
-
         </header>
-        <main className='flex-1'>
+        <main className='flex-1 bg-yellow-200 p-4 flex flex-col justify-center'>
 
           </main>
 
