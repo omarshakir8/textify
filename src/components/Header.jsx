e15dc542f5e2a3f481601b1796cb8157777ed993
omarshakir8 <omarshakir8@gmail.com>
2024-06-22 16:32:13 -0400

    Home Page UI Design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Header() {
   return (
     <header className='flex items-center justify-between gap-4 p-4'>
-    <h3 className= 'text-2xl'>text<span className='text-purple-900'>ify</span></h3>
-    <button className='flex items-center gap-2'>
+    <h1 className= 'text-2xl'>text<span className='text-purple-900 bold'>ify</span></h1>
+    <button className='flex items-center gap-2 text-xl'>
       <p>New</p>
       <i className= "fa-solid fa-plus"></i>
     </button>
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,8 +2,14 @@ import React from "react"
 
 export default function HomePage(){
     return(
-    <main className='flex-1 p-4 flex flex-col gap-3 sm:gap-4 md:gap-5'>
-    Hello there
+    <main className='flex-1 p-4 flex flex-col gap-3 text-center sm:gap-4 md:gap-5 justify-center'>
+   <h1 className= 'font-semibold text-7xl sm:text-8xlmd: text-9xl'>text<span className='text-purple-900 bold'>ify</span></h1>
+   <h3 className="font-semibold text-2xl">Record <span className='text-purple-900'>&rarr;</span> Transcribe<span className='text-purple-900'>&rarr;</span>Translate</h3>
+   <button>
+    <p>Record</p>
+    <i className="fa-solid fa-microphone"></i>
+   </button>
+
     </main>
 )
 }
